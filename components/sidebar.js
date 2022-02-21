0147db3c192bba1b308826d5da1e7a237523af52
manjeetsingh <manjeetsingh100001@yahoo.com>
2022-02-21 17:50:55 +0530

    masai news

---
+++ components/sidebar.js
@@ -0,0 +1,15 @@
+function sidebar() {
+return `
+<h1>Masai news</h1>
+<input type="text" id="inp" placeholder="search news">
+<p>startup</p>
+<p>Newsletter</p>
+<p>Audio</p>
+<p>Video</p>
+
+`;
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+}
+export default sidebar
---
+++ scripts/search.js
@@ -0,0 +1,5 @@
+function storeSearchterm(term) {
+
+
+export default storeSearchterm
+
